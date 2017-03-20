6c857d191dea7d1714a15a221e66c8b774f07d6d
aajjapla_c <anildvg2@gmail.com>
2017-03-20 15:53:38 -0700

    initial project commit!

---
+++ omnichannel/gradle_build.bat
@@ -0,0 +1,10 @@
+title gradle build
+SET JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_121
+SET PATH=%PATH%;C:\Anil\Gradle\gradle-3.4.1\bin;
+
+REM gradle build --stacktrace
+REM gradle wrapper --gradle-version 3.4.1
+
+gradlew build
+
+pause
